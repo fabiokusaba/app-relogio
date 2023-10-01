cc0d5cd60517be9ee98bfa0fd0dcfc37ba73f589
Fabio Kusaba <fabinhokb44@gmail.com>
2023-09-30 21:32:41 -0300

    Aplicativo relógio de mesa - configurações iniciais

---
+++ app/src/main/java/com/fabiokusaba/apprelogio/FullscreenActivity.kt
@@ -0,0 +1,26 @@
+package com.fabiokusaba.apprelogio
+
+import android.os.Build
+import android.os.Bundle
+import android.view.WindowInsets
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+import com.fabiokusaba.apprelogio.databinding.ActivityFullscreenBinding
+
+class FullscreenActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityFullscreenBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityFullscreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        }
+        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
+    }
+}
